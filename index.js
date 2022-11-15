3abe8d5986347a88fc0cf1d332d5b1fd548f6900
Carlos <carlos@marah.dev>
2022-11-15 08:50:51 +0100

    Use PORT env variable

--- index.js
+++ index.js
@@ -44,6 +44,6 @@ app.get('/', async (req, res) => {
   res.send(links);
 });
 
-app.listen(port, () => {
+app.listen(process.env.PORT || port, () => {
   console.log(`Example app listening on port ${port}`)
 });
